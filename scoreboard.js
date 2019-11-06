f00f215a41e3eb5fd45b47eb11cb85cf2f343c51
kikibres <kristinabressler@yahoo.com>
2019-11-06 09:25:12 -0500

    scoreboard

--- scoreboard.js
+++ scoreboard.js
@@ -1,56 +1,33 @@
 let high_scores = document.querySelector("ol.high-scores");
-let current_score = sessionStorage.getItem('score');
+let scores = new Array(10); //create an empty array with 10 slots
 
-// High scores scoreboard
-function HighScores() {
-  // if(typeof(Storage)!=="undefined"){
-      let scoreslist = false;
-      if(localStorage["high-scores"]) {
-          high_scores.style.display = "block";
-          high_scores.innerHTML = '';
-          scoreslist = JSON.parse(localStorage["high-scores"]);
-          scoreslist = scoreslist.sort(function(a,b){return parseInt(b)-parseInt(a)});
-
-          for(let i = 0; i < 10; i++){
-              let s = scoreslist[i];                        
-              let fragment = document.createElement('li');
-              fragment.innerHTML = (typeof(s) != "undefined" ? s : "" );
-              console.log(fragment)
-              high_scores.appendChild(fragment);
-          }
-      }
-  // }
+for(let i=0; i<scores.length;i++){ // add empty string to the array.
+  scores[i] = "";
 }
-HighScores();
-function UpdateScore() {
-  // if(typeof(Storage)!=="undefined"){
-      let current = current_score; //get current score
-      let scoreslist = false;
-      if(localStorage["high-scores"]) {
+// console.log(scores);
 
-          scoreslist = JSON.parse(localStorage["high-scores"]);
-          scoreslist = scores.sort(function(a,b){return parseInt(b)-parseInt(a)});
-          
-          for(let i = 0; i < 10; i++){
-              let s = parseInt(scoreslist[i]);
-              
-              let val = (!isNaN(s) ? s : 0 );
-              if(current > val)
-              {
-                  val = current;
-                  scoreslist.splice(i, 0, parseInt(current));
-                  break;
-              }
-          }
-          
-          scoreslist.length = 10;                                
-          localStorage["high-scores"] = JSON.stringify(scoreslist);
+sessionStorage["high-scores"] = JSON.stringify(scores); //add the array to sessionStorage. Will change to localStorage later
+// console.log(sessionStorage["high-scores"]);
 
-      } else {                        
-          let scoreslist = new Array();
-          scoreslist[0] = current;
-          localStorage["high-scores"] = JSON.stringify(scoreslist);
-      }
-      HighScores();
-  // } 
+function highScores() { //populate the <li> with array above
+  if(sessionStorage["high-scores"]) { //if the sessionStorage have the array which I already created.
+			high_scores.style.display = "block";
+			high_scores.innerHTML = '';
+			scores = JSON.parse(sessionStorage["high-scores"]);
+			console.log(scores);
+			scores = scores.sort(function(a,b){return parseInt(b)-parseInt(a)});
+
+						for(var i = 0; i < 10; i++){
+							var s = scores[i];						
+							var fragment = document.createElement('li');
+							fragment.innerHTML = (typeof(s) != "undefined" ? s : "" );
+							high_scores.appendChild(fragment);
+						}
+					}
+}
+
+highScores();
+
+function updateScores() {
+  
 }
