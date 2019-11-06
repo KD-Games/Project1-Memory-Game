45e0dc2af22b4e14406749f1431ab1e79c109b49
kikibres <kristinabressler@yahoo.com>
2019-11-06 11:54:27 -0500

    nothing to report

--- scoreboard.js
+++ scoreboard.js
@@ -6,20 +6,20 @@ for(let i=0; i<scores.length;i++){ // add empty string to the array.
 }
 // console.log(scores);
 
-sessionStorage["high-scores"] = JSON.stringify(scores); //add the array to sessionStorage. Will change to localStorage later
+localStorage["high-scores"] = JSON.stringify(scores); //add the array to sessionStorage. Will change to localStorage later
 // console.log(sessionStorage["high-scores"]);
 
 function highScores() { //populate the <li> with array above
-  if(sessionStorage["high-scores"]) { //if the sessionStorage have the array which I already created.
+  if(localStorage["high-scores"]) { //if the sessionStorage have the array which I already created.
 			high_scores.style.display = "block";
 			high_scores.innerHTML = '';
-			scores = JSON.parse(sessionStorage["high-scores"]);
+			scores = JSON.parse(localStorage["high-scores"]);
 			console.log(scores);
 			scores = scores.sort(function(a,b){return parseInt(b)-parseInt(a)});
 
 						for(var i = 0; i < 10; i++){
-							var s = scores[i];						
-							var fragment = document.createElement('li');
+							let s = scores[i];						
+							let fragment = document.createElement('li');
 							fragment.innerHTML = (typeof(s) != "undefined" ? s : "" );
 							high_scores.appendChild(fragment);
 						}
@@ -29,5 +29,16 @@ function highScores() { //populate the <li> with array above
 highScores();
 
 function updateScores() {
-
+  let current = score;
+  scores = JSON.parse(localStorage["high-scores"]);
+  for (let i = 0; i < scores.length; i++) {
+    if (current >= scores[i]) {
+      scores.splice(i, 0, current);
+      scores.length = Math.min(scores.length, 10); //Where 10 is the max scores you want to keep
+      break; //stop the loop
+    }
+  }
+  localStorage["high-scores"] = JSON.stringify(scores);
+  highScores();
 }
+updateScores();
